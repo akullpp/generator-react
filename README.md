3281dbaf0562e55fb72ffdb15782f9043929f375
Andreas Kull <akullpp@gmail.com>
2019-12-04 09:54:50 +0100

    Initialize

---
+++ README.md
@@ -0,0 +1,30 @@
+# Generator for React Projects
+
+## Usage
+
+```sh
+git clone
+cd generator-react
+npm link
+yo react
+```
+
+## Stack
+
+### Production
+
+- axios
+- create-react-app
+- emotion core
+- material-ui with icons
+- react-dom
+- react-router
+
+### Development
+
+- concurrently
+- editorconfig
+- eslint with Airbnb
+- husky
+- prettier
+- react-testing-library
---
+++ generators/app/index.js
@@ -0,0 +1,67 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+  constructor(args, options) {
+    super(args, options)
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Project name',
+        validate: input => Boolean(input.length),
+      },
+    ])
+
+    this.destinationRoot(this.answers.projectName)
+  }
+
+  writing() {
+    this.log('Copying templates...')
+
+    this.fs.copy(this.templatePath('static/**/*'), this.destinationPath(), {
+      globOptions: { dot: true },
+    })
+
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      this.answers,
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
+      this.answers,
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('LICENSE'),
+      this.destinationPath('LICENSE'),
+      { year: new Date().getFullYear() },
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      this.answers,
+    )
+  }
+
+  end() {
+    this.log('Updating dependencies...')
+    this.spawnCommandSync('npx', ['ncu', '-u'])
+
+    this.log('Installing dependencies...')
+    process.chdir
+    this.spawnCommandSync('npm', ['i'])
+
+    this.log('Setting up git...')
+    this.spawnCommandSync('git', ['init'])
+    this.spawnCommandSync('git', ['checkout', '-b', 'develop'])
+    this.spawnCommandSync('git', ['add', '.'])
+    this.spawnCommandSync('git', ['commit', '-am', '"Initialize"'])
+  }
+}
