1ef4a67ebfa6009ef9e93d09cefd948154a97c36
Andreas Kull <akullpp@gmail.com>
2019-12-04 14:06:20 +0100

    Add CSS normalization

--- generators/app/templates/static/src/index.js
+++ generators/app/templates/static/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
+import './index.css'
 import App from './App'
 
 axios.defaults.baseURL = process.env.REACT_APP_API
