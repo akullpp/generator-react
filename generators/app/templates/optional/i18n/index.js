34213937dc94543c0c229350f773606c37fa6903
Andreas Kull <andreas.kull@daimler.com>
2020-01-21 11:56:19 +0100

    Add normalize.css as node module

--- generators/app/templates/optional/i18n/index.js
+++ generators/app/templates/optional/i18n/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
+import 'normalize.css'
 
-import './i18n'
-import './index.css'
 import App from './App'
+import { GlobalStyle } from './theme'
+import './i18n'
 
 axios.defaults.baseURL = process.env.REACT_APP_API
 axios.interceptors.response.use(response => response.data)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+)
--- generators/app/templates/static/src/index.js
+++ generators/app/templates/static/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
+import 'normalize.css'
 
-import './index.css'
 import App from './App'
+import { GlobalStyle } from './theme'
 
 axios.defaults.baseURL = process.env.REACT_APP_API
 axios.interceptors.response.use(response => response.data)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+)
